bd85aa7171e00853576e158d5236589a710f3b75
AdrianAlvarez25 <151928540+AdrianAlvarez25@users.noreply.github.com>
2024-02-22 10:25:32 +0100

    Create fraseconmasas.py
    desglosamiento de funciones

---
+++ PhraseWithMoreA/fraseconmasas.py
@@ -0,0 +1,13 @@
+''' 
+Funcion principal: main
+    Pedirle al usuario que escriba una frase
+Funcion: obtener frase con mas a
+    Buscar el maximo de a's que hay en la frase
+    Contar las a's que hay en la frase
+    Recibir el resultado final (la cantidad de a's que hay)
+Funcion:Contar letras a
+    Contar las a's que hay en la frase
+Funcion: Mostrar resultado
+    Recibir la frase con mas a's
+    Recibir la cantidad de a's en la frase
+'''
