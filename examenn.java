960a20ae073f7d32dfa3a8666e9222f6765d7f1c
AdrianAlvarez25 <151928540+AdrianAlvarez25@users.noreply.github.com>
2024-05-10 12:20:16 +0200

    primer commit

---
+++ examenn.java
@@ -0,0 +1,8 @@
+// voy a ir haciendo commits seguidos mas o menos de como he ido haciendolo ya que no me estaba dejando entrar al github en toda la hora
+public class Personatge {
+    private String nom;
+    private int nivell;
+    private int puntsDeVida;
+    private int puntsDeMana;
+    private String arma;
+    private String armadura;
